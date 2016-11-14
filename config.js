94147b2d3ba853222af3ad8e08d9c2770d78c58e
Muckbuck <rosenborg.eric@gmail.com>
2016-11-14 19:44:04 +0100

    I dun goofed

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
 module.exports = {
-    appSecret: "2b77e36e20ecaef603d50969c4a18fb8",
-    appId: "325366134503588"
+    fbOptions: {
+        appSecret: "2b77e36e20ecaef603d50969c4a18fb8",
+        appId: "325366134503588"
+    },
+    mlabOptions: {
+        username: "admin",
+        password: "admin123"
+    }
+    
 }
--- routes/api/fbPosts.js
+++ routes/api/fbPosts.js
@@ -1,9 +1,10 @@
 module.exports = function(app){
     app.get('/api/fbposts', function(req,res){
+        var postsLimit = req.query.limit;
         req.facebook.api('/61569713341/posts', function(err, pagePosts) {
             console.log(pagePosts)
             
-            res.send(pagePosts);
+            res.send(pagePosts.data);
         });
     })
     
