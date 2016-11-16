9fa7ea982736866efbefaf54e17470ec7704b36d
Muckbuck <rosenborg.eric@gmail.com>
2016-11-16 15:38:21 +0100

    Changed port settings

--- routes/api/fbPosts.js
+++ routes/api/fbPosts.js
@@ -1,12 +1,20 @@
 module.exports = function(app){
     app.get('/api/fbposts', function(req,res){
         var postsLimit = parseInt(req.query.limit);
-
-        req.facebook.api('/61569713341/posts?limit=' + postsLimit, function(err, pagePosts) {
-            console.log(pagePosts)
-            
-            res.send(pagePosts.data);
-        });
+        if(!postsLimit){
+            req.facebook.api('/61569713341/posts', function(err, pagePosts) {
+                console.log(pagePosts)
+                
+                res.send(pagePosts.data);
+            });
+        }else{
+            req.facebook.api('/61569713341/posts?limit=' + postsLimit, function(err, pagePosts) {
+                console.log(pagePosts)
+                
+                res.send(pagePosts.data);
+            });
+        }
+        
     })
     
 }
